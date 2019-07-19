b71f1dc1b36fab7fcf3bc6b0f01d66a778190b19
Abdulaziz1337 <53045352+1337Devil@users.noreply.github.com>
2019-07-19 09:09:09 +0300

    Update RedEye.py

--- RedEye.py
+++ RedEye.py
@@ -1,4 +1,3 @@
-
 # Coded By 1337Devil :)
 # import os command ..
 import os
@@ -8,25 +7,28 @@ import pyfiglet
 
 ascii_banner = pyfiglet.figlet_format("1337Devil")
 print(ascii_banner)
-print("                   Thanks To Mr.hEx020")
+print("                   Thanks To Mr.hEx")
 print("                   Thanks To ThEhilL")
 print("                   Greets To My Friends")
 
 
 def a():
-    return os.system('{/***/ca*,/**c/****wd}')
+        return os.system('{/***/ca*,/**c/****wd}')
 def b():
-    return os.system('/***/[c][a]* /**[c]/*****[d]')
+        return os.system('/***/[c][a]* /**[c]/*****[d]')
 def c():
-    return os.system('/***/ca*,/**c/****ow}')
-def op(x) :
-    switcher={
-        1: a(),
-        2: b(),
-        3: c(),
-    }
-    return switcher.get(x, "your option not available")
+        return os.system('/***/ca*,/**c/****ow}')
+
+
 print("Press 1 Or 2 to read passwd file , and 3 to read shadow file ")
 n=int(input("Enter your Choice: "))
-c=op (n)
-print(c)
+
+if n == 1:
+      a()
+elif n == 2:
+      b()
+elif n == 3:
+      c()
+else:
+   print("your option not available")
+
